9eff71d2e9a21b38522be1662dff274bd23a4825
dustinpfister <dustin.pfister@gmail.com>
2018-01-04 11:37:02 -0500

    using a data object for a plugin

--- scripts/demo-gen-with-posts-and-layout.js
+++ scripts/demo-gen-with-posts-and-layout.js
@@ -7,11 +7,15 @@ generate with data from posts
 
 hexo.extend.generator.register('gen-with-posts-layout', function (locals) {
 
+    console.log(locals);
+
+    locals.data.report = true;
+
     return [{
 
             path: 'with-posts-layouts/index.html',
             data: locals,
-            layout: ['index']
+            layout: ['report']
 
         }
 
